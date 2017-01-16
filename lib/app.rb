f992552832ff8d144e843126afc53f890268b175
olwend <olwend@gmail.com>
2017-01-16 16:47:16 +0000

    Intitial html

---
+++ lib/app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+get '/' do
+  "Hello!"
+end
+get '/secret' do
+  "you can\'t see this."
+end
+
+get '/cat' do
+"<div style='border: 3px dashed red'>
+ <img src='http://bit.ly/1eze8aE'>
+</div>"
+
+
+end
+
+set :session_secret, 'super secret'
