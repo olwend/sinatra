3346d44ca2fafe29d2dd065b4cb53a00b6b04d41
Joe Marriott <marriott90@gmail.com>
2017-01-17 13:23:51 +0000

    sinatra: using forms

--- lib/app.rb
+++ lib/app.rb
@@ -3,19 +3,20 @@ require 'sinatra'
 get '/' do
   "Hello!"
 end
+
 get '/secret' do
   "you can\'t see this."
 end
 
 get '/random-cat' do
-@name=["Amigo", "Oscar", "Viking"].sample
-erb(:index)
+  @name=["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
 
 get '/named-cat' do
-p params
-@name=params[:name]
-erb(:index)
+  p params
+  @name=params[:name]
+  erb(:index)
 end
 
 set :session_secret, 'super secret'
