3c14b4404da0c25be3418e641666e5a695d79a2a
olwend <olwend@gmail.com>
2017-01-17 12:28:34 +0000

    12. params

--- lib/app.rb
+++ lib/app.rb
@@ -7,8 +7,14 @@ get '/secret' do
   "you can\'t see this."
 end
 
-get '/cat' do
+get '/random-cat' do
 @name=["Amigo", "Oscar", "Viking"].sample
+erb(:index)
+end
+
+get '/named-cat' do
+p params
+@name=params[:name]
 erb(:index)
 end
 
